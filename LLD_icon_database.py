ca5b98b7e583d101074557f541b8e1a89f972815
Karan Chadha <kchadha291@gmail.com>
2019-05-16 19:16:45 -0400

    Code to generate contour/zernike database

--- LLD_icon_database.py
+++ LLD_icon_database.py
@@ -15,14 +15,14 @@ from sklearn.preprocessing import normalize
 
 ## simple way to load the complete dataset (for a more sophisticated generator example, see LLD-logo script)
 ## open hdf5 file
-#hdf5_file = h5py.File('LLD-icon.hdf5', 'r')
+hdf5_file = h5py.File('LLD-icon.hdf5', 'r')
 ## load data into memory as numpy array
 ##images, labels = (hdf5_file['data'][:], hdf5_file['labels/resnet/rc_64'][:])
 #
 ## alternatively, h5py objects can be used like numpy arrays without loading the whole dataset into memory:
-#images, labels = (hdf5_file['data'], hdf5_file['labels/resnet/rc_64'])
+images, labels = (hdf5_file['data'], hdf5_file['labels/resnet/rc_64'])
 ## here, images[0] will be again returned as a numpy array and can eg. be viewed with matplotlib using
-#images = images[:10000]
+images = images[:10000]
 
 # save and load pickle objects 
 def save_obj(obj, name ):
