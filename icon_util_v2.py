c2fe06c22e3fa130b73dfa63d2ec023965b423bd
tyaglot <y.tolga@nyu.edu>
2021-09-25 09:03:39 -0700

    Fix indentation

--- icon_util_v2.py
+++ icon_util_v2.py
@@ -160,12 +160,10 @@ def prep_img(img,gray=True):
     return img
 
 def adjust_gamma(image, gamma=1.0):
-
-   invGamma = 1.0 / gamma
-   table = np.array([((i / 255.0) ** invGamma) * 255
-      for i in np.arange(0, 256)]).astype("uint8")
-
-   return cv2.LUT(image, table)
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+        for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, table)
 
 
 ## all the functions related to zernike and contour
